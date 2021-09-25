2895d3e08238ccea53be05e2321fc7fd5a2d614e
Admer <49600504+Admer456@users.noreply.github.com>
2021-09-25 16:19:02 +0200

    Dictionary: comments

--- src/Containers/Dictionary.hpp
+++ src/Containers/Dictionary.hpp
@@ -1,12 +1,36 @@
 
 #pragma once
 
-// Game entity dictionary for keyvalues
-
 namespace adm
 {
 	class Vec3;
 
+	/*
+		Example usage:
+		Dictionary dict;
+		dict.SetFloat( "health", 100.0f );
+		...
+		float health = dict.GetFloat( "health" );
+		
+		OR
+		
+		float health = 0.0f;
+		if ( dict.GetFloat( "health", health )
+		{
+			// the key has been found
+		}
+
+		OR
+
+		if ( dict.KeyExists( "health" ) )
+		{
+			float health = dict.GetHealth( "health" );
+		}
+	*/
+
+	// ============================
+	// Game entity dictionary for keyvalues
+	// ============================
 	class Dictionary final
 	{
 	public:
@@ -15,31 +39,33 @@ namespace adm
 		Dictionary( const Dictionary& dict );
 		~Dictionary();
 
+		// C++ strings
 		std::string GetString( const char* keyname, const char* default = "" ) const;
 		bool 		GetString( const char* keyname, std::string& out ) const;
 		void 		SetString( const char* keyname, std::string_view value );
-
+		// C strings
 		const char* GetCString( const char* keyname, const char* default = "" ) const;
 		bool 		GetCString( const char* keyname, char* out, int length ) const;
 		void 		SetCString( const char* keyname, const char* value );
-		
+		// Floats
 		float 		GetFloat( const char* keyname, const float& default = 0.0f ) const;
 		bool 		GetFloat( const char* keyname, float& out ) const;
 		void 		SetFloat( const char* keyname, float value );
-		
+		// Integers
 		int 		GetInteger( const char* keyname, const int& default = 0 ) const;
 		bool 		GetInteger( const char* keyname, int& out ) const;
 		void 		SetInteger( const char* keyname, int value ) ;
-
+		// Booleans
 		bool 		GetBool( const char* keyname, const bool& default = false ) const;
 		bool 		GetBool( const char* keyname, bool& out ) const;
 		void 		SetBool( const char* keyname, bool value );
-
+		// Vec3
 		Vec3		GetVec3( const char* keyname, const Vec3& default = Vec3::Zero ) const;
 		bool		GetVec3( const char* keyname, Vec3& out ) const;
 		void		SetVec3( const char* keyname, const Vec3& value );
-
+		// Does this key exist?
 		bool		KeyExists( const char* keyname ) const;
+		// Clears all keyvalue pairs
 		void		Clear();
 
 		std::string_view operator[] ( const char* keyname )
