662880c8a4e1acb85ef17fac46c994ca39874a0d
Admer <49600504+Admer456@users.noreply.github.com>
2021-09-25 16:20:41 +0200

    Platform: platform constants

--- src/Platform.hpp
+++ src/Platform.hpp
@@ -6,4 +6,21 @@ namespace adm
     #else
     constexpr bool Debug = true;
     #endif
+
+    // These aren't used in AdmUtils, since the code should be
+    // portable to all platforms specified below, BUT, it may be
+    // useful to whoever uses this library
+    struct Platforms
+    {
+        static constexpr int Windows = 0;
+        static constexpr int Linux = 1;
+    };
+
+    #ifdef WIN32
+    constexpr int Platform = Platforms::Windows;
+    #endif
+
+    #ifdef __linux__
+    constexpr int Platform = Platforms::Linux;
+    #endif
 }
