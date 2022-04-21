1733dfec48e320b814218defea0cf33c68fd7e6e
admer456-stu <admer.suko@edu.fit.ba>
2022-04-21 14:22:29 +0200

    Rename InstanceChain to Chain

---
+++ src/Containers/Chain.hpp
@@ -0,0 +1,43 @@
+
+#pragma once
+
+namespace adm
+{
+    // A linked list template for classes that will add their own instances to the list. 
+    // You typically would not use this like a standard linked list, hence the name chain.
+    // T must inherit from Chain
+    template<typename T>
+    class Chain
+    {
+    public:
+        Chain()
+        {
+            prev = head;
+            head = static_cast<T*>(this);
+        }
+
+        // TODO: make this a double linked list,
+        // so we can easily remove this from the chain
+        // in the destructor
+
+        static T* GetHead()
+        {
+            return head;
+        }
+
+        using ProcessFn = void( T* object );
+        static void DoAll( ProcessFn* function )
+        {
+            T* current = head;
+            while ( current )
+            {
+                function( current );
+                current = current->prev;
+            }
+        }
+
+    protected:
+        inline static T* head = nullptr;
+        T* prev = nullptr;
+    };
+}
