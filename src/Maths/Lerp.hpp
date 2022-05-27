996b01313d2a7ae224064e87abf884718c045ae6
admer456-stu <admer.suko@edu.fit.ba>
2022-05-27 09:06:52 +0200

    Lerp: fix Fade, templatise everything

--- src/Maths/Lerp.hpp
+++ src/Maths/Lerp.hpp
@@ -4,15 +4,21 @@
 namespace adm
 {
     template<typename T>
-    inline T Square( const T& x )
+    constexpr T Square( const T& x )
     {
         return x * x;
     }
 
-    // Linear interpolation between 'a' and 'b'
-    // @param alpha: goes from 0.0 (a) to 1.0 (b)
     template<typename T>
-    inline T Lerp( const T& a, const T& b, const float& alpha )
+    constexpr T Cube( const T& x )
+    {
+        return x * x * x;
+    }
+
+    // Linear interpolation between 'a' and 'b'
+    // @param alpha: Blend factor, from 0.0 (a) to 1.0 (b)
+    template<typename T>
+    constexpr T Lerp( const T& a, const T& b, const float& alpha )
     {
         return (a * 1.0f - alpha) + (b * alpha);  
     }
@@ -20,43 +26,50 @@ namespace adm
     // Non-linear transforms
 
     // Damp interpolation between 'source' and 'target'
-    // You can use this instead of a = a*0.9 + a*0.1
-    // Doesn't require normalised values except for rate
-    // @param rate: How fast to transition
-    inline float Fade( const float& source, const float& target, const float& rate, const float& deltaTime )
+    // You can use this instead of a = a*0.9 + b*0.1
+    // @param source: The source value
+    // @param target: The target to eventually reach
+    // @param rate: How fast to transition [0,1]
+    template<typename T>
+    constexpr T Fade( const T& source, const T& target, const float& rate, const float& deltaTime )
     {
-        return Lerp( source, target, std::pow( rate, deltaTime ) );
+        return Lerp( target, source, std::pow( rate, deltaTime ) );
     }
 
     // Normalised non-linear transforms
+    // Todo: links to graphs for each of these
 
     // Ramp, x^2
-    inline float Ramp2( const float& x )
+    template<typename T>
+    constexpr T Ramp2( const T& x )
     {
         return x * x;
     }
 
     // Ramp, x^3
     // Steeper than Ramp2
-    inline float Ramp3( const float& x )
+    template<typename T>
+    constexpr T Ramp3( const T& x )
     {
         return x * x * x;
     }
 
     // Smooth step with bounce
-    inline float SmoothStep2Bounce( const float& x, const float& bounceFactor = 1.0f )
+    template<typename T, typename T2>
+    inline T SmoothStep2Bounce( const T& x, const T2& bounceFactor = 1.0f )
     {
-        float rising = 2.0f * bounceFactor * (x * x);
-        float reaching = -2.0f * bounceFactor * Square( x - 1.0f ) + 1.0f;
+        const T rising = 2.0f * bounceFactor * (x * x);
+        const T reaching = -2.0f * bounceFactor * Square( x - 1.0f ) + 1.0f;
 
         return Lerp( rising, reaching, x );
     }
 
     // Smooth step
-    inline float SmoothStep2( const float& x )
+    template<typename T>
+    inline float SmoothStep2( const T& x )
     {
-        float rising = x * x;
-        float reaching = -Square( x - 1.0f ) + 1.0f;
+        const T rising = Square( x );
+        const T reaching = -Square( x - 1.0f ) + 1.0f;
 
         return Lerp( rising, reaching, x );
     }
