8232fea4d9e2586806628d72b53f8b7024d558a8
Admer <49600504+Admer456@users.noreply.github.com>
2022-09-23 23:29:26 +0200

    Platform: finally discern between X11 and Wayland; move ADM_API etc. into Platform.hpp

--- src/Platform.hpp
+++ src/Platform.hpp
@@ -49,6 +49,41 @@ namespace adm
 
 #elif ADM_PLATFORM == PLATFORM_LINUX
 
-// Add X11 and other stuff here
+#if ADM_USE_X11
+#include <X11/Xlib.h>
+#endif
+
+#if ADM_USE_WAYLAND
+#include <wayland-client-core.h>
+#endif
 
 #endif
+
+// Visual Studio 2017 15.3 supports C++17 AFAIK
+#if defined(_MSC_VER) && _MSC_VER > 1911
+#define ADM_EXPORT __declspec(dllexport)
+
+// Stupid stupid hack to work around a really really
+// stupid MSVC detail
+#define ADM_CPP _MSVC_LANG
+
+// GCC 5 onwards supports C++17
+#elif __GNUC__ >= 5 || defined(__clang__)
+// https://gcc.gnu.org/wiki/Visibility
+#define ADM_EXPORT __attribute__((visibility("default")))
+
+#define ADM_CPP __cplusplus
+
+#else
+
+#if defined(_MSC_VER)
+#error "Your MSVC compiler is outdated, you need something from 2017 and newer"
+#elif __GNUC__ < 5
+#error "Your G++ is outdated, you need at least GCC 5 or newer with C++17 support"
+#else
+#error "Compiler not supported"
+#endif
+
+#endif
+
+#define ADM_API extern "C" ADM_EXPORT
--- src/Precompiled.hpp
+++ src/Precompiled.hpp
@@ -95,18 +95,3 @@ namespace adm
 
 // System-interfacing stuff
 #include "System/Library.hpp"
-
-// Visual Studio 2017 15.3 supports C++17 AFAIK
-#if defined(_MSC_VER) && _MSC_VER > 1911
-#define ADM_EXPORT __declspec(dllexport)
-
-// GCC 5 onwards supports C++17
-#elif __GNUC__ >= 5 || defined(__clang__)
-// https://gcc.gnu.org/wiki/Visibility
-#define ADM_EXPORT __attribute__((visibility("default")))
-
-#else
-#error "Compiler not supported"
-#endif
-
-#define ADM_API extern "C" ADM_EXPORT
